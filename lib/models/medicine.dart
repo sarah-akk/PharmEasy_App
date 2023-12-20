0b6e6d9615ddeeb65c74ff2c3f667e80ce1cf125
sara_akk <sarah.m.akkad@gmail.com>
2023-12-20 09:12:54 -0500

    editing

--- lib/models/medicine.dart
+++ lib/models/medicine.dart
@@ -2,13 +2,13 @@ import 'package:flutter/foundation.dart';
 
 class Medicine with ChangeNotifier {
 
-  final  id;
+  final  int id;
   final String scientificName;
   final String commercialName;
   final String category;
   final String manufacturer;
-  final int quantityAvailable;
-  final DateTime expiryDate;
+  final double quantityAvailable;
+  final String expiryDate;
   final double price;
   final String imageUrl;
   final bool isfavorate;
